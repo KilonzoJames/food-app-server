bbec4dfaaed18cede77074adf3a0bac52a1d0083
KilonzoJames <jay.kilonzo@gmail.com>
2023-11-25 16:16:50 +0300

    Added delete and post methods

--- project/routes/orders.py
+++ project/routes/orders.py
@@ -1,8 +1,9 @@
 from flask_restful import Resource, Api
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
 from ..models.Orders import Order
 from ..models.marshmallow.Ord import OrderSchema
+from ..models.Config import db
 
 order_blue = Blueprint('order_api', __name__)
 order_api = Api(order_blue)
@@ -13,13 +14,67 @@ class OrderResource(Resource):
         order_schema = OrderSchema(many=True)
         json_string = order_schema.dump(orders)
         return {"results": json_string}
+    
+    def post(self):
+        try:
+            # Parse and validate the request data using OrderSchema
+            schema = OrderSchema()
+            validated_data = schema.load(request.json)
 
+            # Create a new order
+            new_order = Order(**validated_data)
+
+            # Add the order to the database
+            db.session.add(new_order)
+            db.session.commit()
+
+            # Return the newly created order
+            result = schema.dump(new_order)
+            return {"result": result}, 201  # HTTP status code for "Created"
+
+        except ValidationError as e:
+            return jsonify(e.messages), 400
+        
 class OrderId(Resource):
     def get(self, id):
         order = Order.query.get_or_404(id)
         schema = OrderSchema()
         return schema.dump(order)
+    
+    def put(self, id):
+        try:
+            # Find the order by ID
+            order = Order.query.get_or_404(id)
 
+            # Parse and validate the request data using OrderSchema
+            schema = OrderSchema()
+            validated_data = schema.load(request.json)
+
+            # Update the order attributes
+            for field, value in validated_data.items():
+                setattr(order, field, value)
+
+            # Commit the changes to the database
+            db.session.commit()
+
+            # Return the updated order
+            result = schema.dump(order)
+            return {"result": result}
+
+        except ValidationError as e:
+            return jsonify(e.messages), 400
+
+    def delete(self, id):
+        # Find the order by ID
+        order = Order.query.get_or_404(id)
+
+        # Delete the order from the database
+        db.session.delete(order)
+        db.session.commit()
+
+        return {"msg": "Order deleted"}
+
+# Add the new resource to the API
 order_api.add_resource(OrderResource, '/orders')
 order_api.add_resource(OrderId, '/orders/<int:id>')
 
